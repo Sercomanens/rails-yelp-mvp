1de97284d6e40e94d5dcebbe58da84dc23ecc904
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-17 13:18:02 +0530

    add validations to models

--- db/migrate/20240517072532_create_reviews.rb
+++ db/migrate/20240517072532_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
       t.string :content
+      t.integer :rating
       t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_064548) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_073908) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_064548) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
+    t.integer "rating"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
